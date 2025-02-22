84d6511f32baf34b43a9c0023b6a92eff91b0881
samuelhan713 <samuelhan.s713@gmail.com>
2025-02-22 15:33:09 -0500

    added navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Container, Box, Heading, Text, Button } from "@chakra-ui/react";
+import Home from "./pages/home";
+import Ingredients from "./pages/ingredients";
+import Profile from "./pages/profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxW="container.lg" p={4}>
+      {/* Header with Navigation */}
+      <Box
+        as="header"
+        py={4}
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Heading as="h1" size="lg">
+          APP NAME
+        </Heading>
+        <Box>
+          <Button as={Link} to="/" variant="link" mr={4}>
+            Home
+          </Button>
+          <Button as={Link} to="/ingredients" variant="link" mr={4}>
+            Ingredients
+          </Button>
+          <Button as={Link} to="/profile" variant="link">
+            Profile
+          </Button>
+        </Box>
+      </Box>
+
+      {/* Routes */}
+      <Box py={4}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ingredients" element={<Ingredients />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Box>
+
+      {/* Footer */}
+      <Box as="footer" py={4} textAlign="center">
+        <Text fontSize="sm">© 2025 My App. All rights reserved.</Text>
+      </Box>
+    </Container>
   );
 }
 
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Home hhiiiii</div>;
+};
+
+export default Home;
